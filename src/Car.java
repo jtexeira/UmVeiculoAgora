5fa6616b7daf410c3503bc79b8c1107f59ba8be5
Hitler <a85504@alunos.uminho.pt>
2019-04-10 01:17:03 +0100

    Cenas

--- src/Car.java
+++ src/Car.java
@@ -10,4 +10,15 @@ public class Car {
 
     private Point position;
     private double range;
+
+    public Car(int type, double avgSpeed, double basePrice, double gasMilage, int rating, Point position, double range) {
+        this.type = type;
+        this.avgSpeed = avgSpeed;
+        this.basePrice = basePrice;
+        this.gasMilage = gasMilage;
+        this.rentalHistoric = new ArrayList<>();
+        this.rating = rating;
+        this.position = position;
+        this.range = range;
+    }
 }
--- src/Owner.java
+++ src/Owner.java
@@ -3,4 +3,8 @@ import java.util.ArrayList;
 public class Owner extends Users{
     private int rating;
     private ArrayList<String> rentalHistoric;
+
+    public Owner() {
+        super(email, name, address, dateOfBirth);
+    }
 }
